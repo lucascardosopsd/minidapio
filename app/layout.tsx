ea24cf0f2a3979e5097a1f924e7d9e48e84d1380
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-26 17:30:52 -0300

    base

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Mini Dápio | Cardápio Digital",
+  description:
+    "Cardápio digital gratuito que atende as necessidades simples do seu estabelecimento",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
