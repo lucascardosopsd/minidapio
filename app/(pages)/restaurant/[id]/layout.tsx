4567b29560c18d930934d10e3895a78e4ce52d37
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-27 17:35:14 -0300

    layout: edit restaurant page base

---
+++ app/(pages)/restaurant/[id]/layout.tsx
@@ -0,0 +1,26 @@
+import Navbar from "@/components/Navbar";
+import { BreadcrumbRouteProps } from "@/types/breacrumb";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+  params: {
+    id: string;
+  };
+}
+
+const Layout = ({ params: { id }, children }: LayoutProps) => {
+  const breadcrumb: BreadcrumbRouteProps[] = [
+    { title: "Início", route: "/dashboard" },
+    { title: "Restaurante", route: `/restaurant/${id}` },
+  ];
+
+  return (
+    <div className="flex flex-col">
+      <Navbar breadcrumb={breadcrumb} />
+      <div className="container">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ app/(pages)/restaurant/[id]/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function Restaurant() {
+  return (
+    <main className="flex items-center justify-center h-[calc(100svh-4rem)] gap-8">
+      Dashboard
+    </main>
+  );
+}
