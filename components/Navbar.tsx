004c5f49a26c04fc2d9cf9216018110ea47bc939
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-27 17:35:27 -0300

    layout: update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,14 +9,19 @@ interface NavbarProps {
 const Navbar = ({ breadcrumb }: NavbarProps) => {
   return (
     <div className="container flex justify-between items-center p-4 border-b border-border h-16">
-      {breadcrumb?.map((route, index) => (
-        <Link href={route.route} className="flex gap-2 text-muted-foreground ">
-          <p className="hover:text-accent-foreground transition">
-            {route.title}
-          </p>
-          <p>/</p>
-        </Link>
-      ))}
+      <div className="flex gap-2">
+        {breadcrumb?.map((route, index) => (
+          <Link
+            href={route.route}
+            className="flex gap-2 text-muted-foreground "
+          >
+            <p className="hover:text-accent-foreground transition">
+              {route.title}
+            </p>
+            <p>/</p>
+          </Link>
+        ))}
+      </div>
       <ThemeToggle />
     </div>
   );
