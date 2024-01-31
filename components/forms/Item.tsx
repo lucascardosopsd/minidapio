a5453362a561094f445e1f55d9c1e4ead6fecb87
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-31 02:17:51 -0300

    fix: price field class names

--- components/forms/Item.tsx
+++ components/forms/Item.tsx
@@ -158,6 +158,9 @@ const ItemForm = ({ defaultValues, toggleOpen = () => {} }: ItemFormProps) => {
             >
               <p>Promoção</p>
               <FieldBuilder
+                name="salePrice"
+                title="Preço Promocional*"
+                type="checkbox"
                 control={form.control}
                 fieldElement={
                   <NumericFormat
@@ -167,12 +170,9 @@ const ItemForm = ({ defaultValues, toggleOpen = () => {} }: ItemFormProps) => {
                     decimalSeparator=","
                     maxLength={8}
                     placeholder="R$0,00"
-                    className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                   />
                 }
-                name="salePrice"
-                title="Preço Promocional*"
-                type="checkbox"
+                fieldClassName="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               />
             </motion.div>
           )}
