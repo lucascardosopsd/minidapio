d29eedfec077a0a3a17726885eb32a0723d41f73
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-02-06 21:44:58 -0300

    fix: scroll smooth

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,14 +21,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className="!scroll-smooth">
       <body className={poppins.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          disableTransitionOnChange
-          enableSystem
-        >
+        <ThemeProvider attribute="class" disableTransitionOnChange enableSystem>
           {children}
           <Toaster />
         </ThemeProvider>
