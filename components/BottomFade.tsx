504adcd17fd2456439325c2e1519e3e7f2d09ddf
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-02-24 16:57:50 -0300

    fix: BottomFade z-index

--- components/BottomFade.tsx
+++ components/BottomFade.tsx
@@ -2,7 +2,7 @@ const BottomFade = ({ className }: { className?: string }) => {
   return (
     <div
       className={
-        "absolute bottom-0 left-0 h-20 w-full bg-gradient-to-t from-background to-transparent cursor-none pointer-events-none" +
+        "absolute bottom-0 left-0 h-20 w-full bg-gradient-to-t from-background to-transparent pointer-events-none z-50" +
         className
       }
     />
