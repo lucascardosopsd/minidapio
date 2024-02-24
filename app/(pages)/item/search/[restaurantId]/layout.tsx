c55c8e5ed8036abf89a695d2e1015fa7e33ace99
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-02-24 15:39:30 -0300

    fix: search page breadcrumb link

--- app/(pages)/item/search/[restaurantId]/layout.tsx
+++ app/(pages)/item/search/[restaurantId]/layout.tsx
@@ -3,6 +3,7 @@ import { fetchUserRestaurantsByQuery } from "@/actions/restaurant/fetchUserResta
 import Navbar from "@/components/Navbar";
 import { BreadcrumbRouteProps } from "@/types/breacrumb";
 import { RestaurantProps } from "@/types/restaurant";
+import { useSearchParams } from "next/navigation";
 import { ReactNode, useEffect, useState } from "react";
 
 interface LayoutProps {
@@ -25,10 +26,16 @@ const Layout = ({ children, params: { restaurantId } }: LayoutProps) => {
     }).then((data) => setRestaurant(data[0]));
   }, []);
 
+  const searchParams = useSearchParams();
+
   const breadcrumb: BreadcrumbRouteProps[] = [
     { title: "Início", route: "/restaurants" },
     { title: restaurant.title, route: "/restaurant/" + restaurantId },
-    { title: "Busca", route: "/item" + "/search" + restaurantId },
+    {
+      title: "Busca",
+      route:
+        "/item" + "/search/" + restaurantId + "?" + searchParams.toString(),
+    },
   ];
 
   return (
