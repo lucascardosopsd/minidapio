a061d946baef9f73a4da2cafb5bb2389de32d304
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-02-25 14:35:29 -0300

    fix: duplicate items for each loop

--- components/dialogs/DuplicateItems.tsx
+++ components/dialogs/DuplicateItems.tsx
@@ -17,7 +17,7 @@ import { createNewItem } from "@/actions/item/createNewItem";
 import { ImSpinner2 } from "react-icons/im";
 
 const DuplicateItemsDialog = () => {
-  const { idList } = useItemStore();
+  const { idList, setAllIds } = useItemStore();
   const pathname = usePathname();
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
@@ -25,28 +25,45 @@ const DuplicateItemsDialog = () => {
   const handleDuplicate = async () => {
     setLoading(true);
     toast(
-      <div className="flex">
+      <div className="flex gap-2 items-center">
         <p>Duplicando itens</p>
         <ImSpinner2 className="animate-spin" />
       </div>
     );
     try {
-      idList.forEach(async (id) => {
-        const { items } = await fetchUserItemsByQuery({ where: { id } });
+      if (idList.length > 1) {
+        for (const id of idList) {
+          const { items } = await fetchUserItemsByQuery({ where: { id } });
 
-        if (items[0]) {
-          const { id: _, ...rest } = items[0];
+          if (items[0]) {
+            const { id: _, ...rest } = items[0];
 
-          await createNewItem(rest, pathname);
+            await createNewItem(rest, pathname);
+          }
+
+          toast(`Item ${items[0].title} duplicado.`);
         }
+        toast("Duplicações completas!");
 
         return;
+      }
+
+      const { items } = await fetchUserItemsByQuery({
+        where: { id: idList[0] },
       });
-      toast("Itens duplicados!");
+
+      if (items[0]) {
+        const { id: _, ...rest } = items[0];
+
+        await createNewItem(rest, pathname);
+      }
+
+      toast(`Item ${items[0].title} duplicado.`);
     } catch (error) {
       toast("Erro ao duplicar itens");
       throw new Error("Error when duplicate items");
     } finally {
+      setAllIds([]);
       setLoading(false);
       setOpen(false);
     }
