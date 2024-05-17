f21d2cb109ddcf42d55da5ba5f293d9e0027feb0
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-05-17 00:00:21 -0300

    update: src folder implemented

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,9 @@ import { withUt } from "uploadthing/tw";
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
