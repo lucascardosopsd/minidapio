5330eec8f86a4231f0ed56cdb3d2d2a6648cb3be
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-05-16 23:49:32 -0300

    fix: next auth secret in production

--- lib/authProviders.ts
+++ lib/authProviders.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GooglePovider from "next-auth/providers/google";
 import prisma from "@/lib/prisma";
+import { NextAuthOptions } from "next-auth";
 
 export const nextAuthOptions = {
   providers: [
@@ -9,5 +10,6 @@ export const nextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  secret: process.env.SECRET,
   adapter: PrismaAdapter(prisma),
-};
+} satisfies NextAuthOptions;
