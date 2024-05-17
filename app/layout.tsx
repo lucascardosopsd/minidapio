b2044f98aca3862281e3a795e052d142d5854d3f
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-05-16 23:54:23 -0300

    layout: default theme set to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
             attribute="class"
             disableTransitionOnChange
             enableSystem
+            defaultTheme="dark"
           >
             {children}
             <Toaster />
