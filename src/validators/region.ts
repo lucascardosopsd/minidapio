93ffae13729cf2cdc01f6a57575eb93e3edbd91f
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-05-17 15:21:19 -0300

    schema: region validator schema

---
+++ src/validators/region.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const regionValidator = z.object({
+  title: z.string({ required_error: "Digite o título" }),
+  state: z.string({ required_error: "Digite o código do estado" }),
+  active: z.boolean().default(true),
+});
