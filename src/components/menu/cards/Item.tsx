f6f2fdcfd7d50773f4a82f5169ebedd38a7f8b3c
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-05-19 22:46:00 -0300

    feat: click on image in item card to magnify

--- src/components/menu/cards/Item.tsx
+++ src/components/menu/cards/Item.tsx
@@ -1,9 +1,10 @@
 import { Card, CardContent } from "@/components/ui/card";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
 import { formatPrice } from "@/tools/formatPrice";
 import { ItemProps } from "@/types/item";
 import Image from "next/image";
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 
 interface ItemCardProps {
   item: ItemProps;
@@ -11,6 +12,8 @@ interface ItemCardProps {
 }
 
 const ItemCard = ({ item, themeColor }: ItemCardProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const cardStyles = { borderColor: themeColor };
 
   const hightLightStyles = {
@@ -19,48 +22,63 @@ const ItemCard = ({ item, themeColor }: ItemCardProps) => {
   } satisfies CSSProperties;
 
   return (
-    <Card
-      className="rounded border w-full"
-      style={item.highlight ? hightLightStyles : cardStyles}
-    >
-      <CardContent className="flex items-center p-0">
-        <Image
-          height={500}
-          width={500}
-          alt="Foto Produto"
-          src={item.image}
-          className="h-32 w-32 object-cover rounded rounded-r-none "
-        />
+    <>
+      <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DialogContent className="flex justify-center items-center">
+          <Image
+            height={1000}
+            width={1000}
+            alt="Foto Produto"
+            src={item.image}
+            className="h-[90%] w-[90%] object-cover rounded rounded-r-none "
+          />
+        </DialogContent>
+      </Dialog>
 
-        <div className={"flex flex-col px-4"}>
-          <p
-            className={cn("font-bold", item.highlight && "text-background")}
-            style={{ color: !item.highlight ? themeColor : "" }}
-          >
-            {item.title}
-          </p>
-          <p className="text-xs">{item.description}</p>
+      <Card
+        className="rounded border w-full"
+        style={item.highlight ? hightLightStyles : cardStyles}
+      >
+        <CardContent className="flex items-center p-0">
+          <Image
+            height={500}
+            width={500}
+            alt="Foto Produto"
+            src={item.image}
+            className="h-32 w-32 object-cover rounded rounded-r-none "
+            onClick={() => setIsModalOpen(true)}
+          />
 
-          {item.sale ? (
-            <>
-              <p className="text-xs line-through">
+          <div className={"flex flex-col px-4"}>
+            <p
+              className={cn("font-bold", item.highlight && "text-background")}
+              style={{ color: !item.highlight ? themeColor : "" }}
+            >
+              {item.title}
+            </p>
+            <p className="text-xs">{item.description}</p>
+
+            {item.sale ? (
+              <>
+                <p className="text-xs line-through">
+                  {formatPrice(item.price, "pt-BR", "BRL")}
+                </p>
+                <p
+                  className={cn("text-md", item.highlight && "text-background")}
+                  style={{ color: !item.highlight ? themeColor : "" }}
+                >
+                  {formatPrice(item.salePrice!, "pt-BR", "BRL")}
+                </p>
+              </>
+            ) : (
+              <p className="font-bold" style={{ color: themeColor }}>
                 {formatPrice(item.price, "pt-BR", "BRL")}
               </p>
-              <p
-                className={cn("text-md", item.highlight && "text-background")}
-                style={{ color: !item.highlight ? themeColor : "" }}
-              >
-                {formatPrice(item.salePrice!, "pt-BR", "BRL")}
-              </p>
-            </>
-          ) : (
-            <p className="font-bold" style={{ color: themeColor }}>
-              {formatPrice(item.price, "pt-BR", "BRL")}
-            </p>
-          )}
-        </div>
-      </CardContent>
-    </Card>
+            )}
+          </div>
+        </CardContent>
+      </Card>
+    </>
   );
 };
 
