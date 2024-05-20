3af550714ddd3b79379965248617ee99eebc31e1
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-05-20 16:03:18 -0300

    update: ad card layout

--- src/components/menu/cards/adCard.tsx
+++ src/components/menu/cards/adCard.tsx
@@ -13,15 +13,13 @@ const AdCard = forwardRef<HTMLDivElement, AdCardProps>(({ ad }, ref) => {
   return (
     <Card ref={ref}>
       <CardContent className="flex flex-col p-0">
-        <div className="h-[130px] w-[350px]">
-          <Image
-            src={ad.image}
-            height={500}
-            width={500}
-            alt="Anúncio"
-            className="w-full h-full rounded object-cover"
-          />
-        </div>
+        <Image
+          src={ad.image}
+          height={500}
+          width={500}
+          alt="Anúncio"
+          className="w-full h-full rounded object-cover"
+        />
         <div className="flex flex-col gap-2 p-5">
           <p className="text-xs text-muted-foreground">Anúncio</p>
           <p className="font-semibold">{ad.title}</p>
