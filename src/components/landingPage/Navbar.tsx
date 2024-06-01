8ae7e32de788dcb981343a62d835a0032e68ca0a
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-06-01 15:11:49 -0300

    fix: navbar responsivity

--- src/components/landingPage/Navbar.tsx
+++ src/components/landingPage/Navbar.tsx
@@ -3,32 +3,71 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { Drawer, DrawerContent, DrawerTrigger } from "../ui/drawer";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="flex border-b w-full px-[16px] tablet:px-[80px] desktop:px-[162px]">
-      <div className="flex items-center justify-between mx-auto w-full p-2">
-        <Image
-          src="/logo-banner.svg"
-          alt="logo"
-          height={500}
-          width={500}
-          className="h-full w-auto"
-        />
+    <>
+      <div className="hidden tablet:flex border-b w-full px-[16px] tablet:px-[80px] desktop:px-[162px] h-20">
+        <div className="flex items-center justify-between mx-auto w-full p-2">
+          <Image
+            src="/logo-banner.svg"
+            alt="logo"
+            height={500}
+            width={500}
+            className="h-full w-auto"
+          />
 
-        <div className="flex items-center gap-5">
-          {/* <Button>Afiliado</Button> */}
+          <div className="flex items-center gap-5">
+            {/* <Button>Afiliado</Button> */}
 
-          <Link href="/advertiser/login">
-            <Button>Anunciante</Button>
-          </Link>
+            <Link href="/advertiser/login">
+              <Button>Anunciante</Button>
+            </Link>
 
-          <Link href="/dashboard/login">
-            <Button>Restaurante</Button>
-          </Link>
+            <Link href="/dashboard/login">
+              <Button>Restaurante</Button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button size="icon" className="absolute right-2 top-2">
+            <Menu size={32} />
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="flex flex-col gap-10 h-[calc(100svh-80px)] p-5 mx-auto">
+            <Image
+              src="/logo-banner.svg"
+              alt="logo"
+              height={500}
+              width={500}
+              className="h-auto w-32 mx-auto"
+            />
+
+            <div className="flex flex-col items-center justify-center gap-10 my-auto h-full ">
+              {/* <Button>Afiliado</Button> */}
+
+              <Link href="/advertiser/login">
+                <p className="text-primary font-semibold border-b text-4xl">
+                  Anunciante
+                </p>
+              </Link>
+
+              <Link href="/dashboard/login">
+                <p className="text-primary font-semibold border-b text-4xl">
+                  Restaurante
+                </p>
+              </Link>
+            </div>
+          </div>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
