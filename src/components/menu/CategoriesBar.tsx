7130339cee23d09f297c56017b549fb3701d8bb8
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-06-08 15:28:58 -0300

    feat: change categories slide by click in arrows

--- src/components/menu/CategoriesBar.tsx
+++ src/components/menu/CategoriesBar.tsx
@@ -3,8 +3,8 @@ import { currentCategoryStore } from "@/context/currentCategory";
 import { cn } from "@/lib/utils";
 import { Category, Item } from "@prisma/client";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { useEffect, useRef } from "react";
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
 
 interface CategoryWithItemProps extends Category {
   items?: Item[];
@@ -17,6 +17,7 @@ interface CategoriesBarProps {
 
 const CategoriesBar = ({ categories, themeColor }: CategoriesBarProps) => {
   const { categoryId, setCategoryId } = currentCategoryStore();
+  const swiperRef = useRef<SwiperClass | null>(null);
 
   const handleSetCategoryId = (id: string) => {
     setCategoryId(id);
@@ -28,36 +29,50 @@ const CategoriesBar = ({ categories, themeColor }: CategoriesBarProps) => {
 
   return (
     <div className="relative">
-      <div className="absolute w-full h-full flex items-center left-0 top-0 justify-between z-50 pointer-events-none text-background">
-        <div className="flex items-center h-full flex-1">
+      <div className="absolute w-full h-full flex items-center left-0 top-0 justify-between z-50 text-background pointer-events-none">
+        <div
+          className="flex items-center h-full w-8 bg-gradient-to-r from-background via-background to-transparent pointer-events-auto"
+          onClick={() => swiperRef?.current?.slidePrev()}
+          style={{
+            color: themeColor,
+          }}
+        >
           <ChevronLeft />
         </div>
 
-        <div className="flex-[2]" />
-
-        <div className="flex items-center h-full flex-1 justify-end">
+        <div
+          className="flex items-center h-full w-8 justify-end pointer-events-auto bg-gradient-to-l from-background via-background to-transparent"
+          onClick={() => swiperRef?.current?.slideNext()}
+          style={{
+            color: themeColor,
+          }}
+        >
           <ChevronRight />
         </div>
       </div>
 
       <Swiper
-        slidesPerView={2}
         style={{
-          background: themeColor,
+          color: themeColor,
           height: "50px",
           padding: "5px",
-          paddingLeft: "50px",
+          paddingLeft: "30px",
+          paddingRight: "30px",
         }}
+        spaceBetween={10}
+        slidesPerView={2}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
       >
         {categories.map((category) => (
           <SwiperSlide key={category.id}>
             <div
               className={cn(
-                "text-background cursor-pointer select-none transition bg-transparent text-center text-sm flex items-center justify-center h-full mx-5 leading-[12px] font-semibold whitespace-nowrap",
-                categoryId == category.id && "bg-background p-2 px-4 rounded-lg"
+                "cursor-pointer select-none transition bg-transparent text-center text-sm flex items-center justify-center h-full leading-[12px] font-semibold whitespace-nowrap",
+                categoryId == category.id && "p-2  rounded-lg"
               )}
               style={{
-                color: categoryId == category.id ? themeColor : "background",
+                color: categoryId == category.id ? "background" : themeColor,
+                background: categoryId == category.id ? themeColor : "",
               }}
               onClick={() => handleSetCategoryId(category.id)}
             >
