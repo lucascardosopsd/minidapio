5a3ca326dd5c2861bd53392a6b34560b337754d5
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-06-09 14:15:26 -0300

    update: landing page menu responsivity

--- src/components/landingPage/Navbar.tsx
+++ src/components/landingPage/Navbar.tsx
@@ -23,13 +23,14 @@ const Navbar = () => {
           <div className="flex items-center gap-5">
             {/* <Button>Afiliado</Button> */}
 
+            <Link href="/dashboard/login">
+              <Button>Restaurante</Button>
+            </Link>
+
             <Link href="https://api.whatsapp.com/send?phone=5517996484654">
               <Button variant="secondary">Anuncie</Button>
             </Link>
 
-            <Link href="/dashboard/login">
-              <Button>Restaurante</Button>
-            </Link>
             <ThemeToggle />
           </div>
         </div>
@@ -38,7 +39,7 @@ const Navbar = () => {
       <Drawer>
         <DrawerTrigger className="flex tablet:hidden">
           <div className="fixed right-2 top-2 p-1 rounded bg-primary">
-            <Menu size={24} />
+            <Menu size={32} />
           </div>
         </DrawerTrigger>
         <DrawerContent>
@@ -54,14 +55,14 @@ const Navbar = () => {
             <div className="flex flex-col items-center justify-center gap-10 my-auto h-full ">
               {/* <Button>Afiliado</Button> */}
 
-              <Link href="/advertiser/login">
-                <p className="text-primary font-semibold border-b text-4xl">
+              <Link href="/advertiser/login" className="order-2 tablet:order-1">
+                <p className="text-primary font-semibold border-b text-4xl ">
                   Anunciante
                 </p>
               </Link>
 
-              <Link href="/dashboard/login">
-                <p className="text-primary font-semibold border-b text-4xl">
+              <Link href="/dashboard/login" className="order-1 tablet:order-2">
+                <p className="text-primary font-semibold border-b text-4xl ">
                   Restaurante
                 </p>
               </Link>
