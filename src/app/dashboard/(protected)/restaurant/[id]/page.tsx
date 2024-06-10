495c7bf31994b597af739e971f6ec8657617a3c6
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-06-10 13:05:24 -0300

    update: categories order in restaurant dashboard

--- src/app/dashboard/(protected)/restaurant/[id]/page.tsx
+++ src/app/dashboard/(protected)/restaurant/[id]/page.tsx
@@ -23,7 +23,7 @@ export default async function Restaurant({
         restaurantId,
       },
       orderBy: {
-        order: "desc",
+        order: "asc",
       },
       include: {
         items: {
