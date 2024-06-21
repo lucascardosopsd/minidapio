8e6a1f4b74380d28ef0244d763500995f273e213
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-06-20 23:39:34 -0300

    update: remove invalid code from error pages

--- src/app/(public)/(landingPage)/error.tsx
+++ src/app/(public)/(landingPage)/error.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { Badge } from "@/components/ui/badge";
-import { useEffect } from "react";
 
 const ErrorPage = (error: { error: Error }) => {
-  useEffect(() => {}, [error]);
-
   return (
     <div className="flex w-full h-[calc(100svh-70px)] flex-col items-center justify-center gap-2">
       <Badge variant="destructive">Erro</Badge>
--- src/app/dashboard/(protected)/item/search/[restaurantId]/error.tsx
+++ src/app/dashboard/(protected)/item/search/[restaurantId]/error.tsx
@@ -1,13 +1,9 @@
 "use client";
-
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { useEffect } from "react";
 
 const ErrorPage = (error: { error: Error }) => {
-  useEffect(() => {}, [error]);
-
   return (
     <div className="flex w-full h-[calc(100svh-70px)] flex-col items-center justify-center gap-2 ">
       <Badge variant="destructive">Erro</Badge>
