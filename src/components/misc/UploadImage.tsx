81aea1eb98f8015affd9975b87a91f29b61822ce
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-07-06 00:40:58 -0300

    fix: image in upload image card

--- src/components/misc/UploadImage.tsx
+++ src/components/misc/UploadImage.tsx
@@ -1,6 +1,12 @@
 import { toast } from "sonner";
 import { UploadDropzone } from "./UploadThing";
-import { FormControl, FormField, FormItem, FormMessage } from "../ui/form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 import { Control, useWatch } from "react-hook-form";
 import Image from "next/image";
 
@@ -16,43 +22,45 @@ const UploadImage = ({ control, name }: UploadImageProps) => {
   });
 
   return (
-    <div className="relative rounded overflow-hidden">
-      {watchChange && (
-        <Image
-          src={watchChange}
-          alt={name}
-          width={500}
-          height={500}
-          className="h-full w-full object-cover absolute bottom-0 -z-10 opacity-50"
-        />
-      )}
-
+    <div className="rounded overflow-hidden">
       <FormField
         control={control}
         name={name}
         render={({ field: { onChange } }) => (
           <FormItem>
+            <FormLabel>Upload da imagem</FormLabel>
             <FormControl>
-              <UploadDropzone
-                content={{
-                  label: "Enviar arquivo",
-                  allowedContent: "Imagem de até 1MB",
-                  button: "Confirmar",
-                }}
-                endpoint="imageUploader"
-                onClientUploadComplete={(res) => onChange(res[0].url)}
-                onUploadError={(error: Error) =>
-                  toast("Erro ao enviar a logo.")
-                }
-                appearance={{
-                  label: "text-primary hover:text-primary/60 transition",
-                  button: "bg-primary text-background",
-                  allowedContent: "text-primary/60",
-                  uploadIcon: "text-primary",
-                  container: "w-full h-full -mt-2",
-                }}
-                className="border border-primary hover:border-primary/60"
-              />
+              <div className="relative">
+                {watchChange && (
+                  <Image
+                    src={watchChange}
+                    alt={name}
+                    width={500}
+                    height={500}
+                    className="h-full w-full object-cover absolute bottom-0 -z-10 opacity-50 rounded-lg"
+                  />
+                )}
+                <UploadDropzone
+                  content={{
+                    label: "Enviar arquivo",
+                    allowedContent: "Imagem de até 1MB",
+                    button: "Confirmar",
+                  }}
+                  endpoint="imageUploader"
+                  onClientUploadComplete={(res) => onChange(res[0].url)}
+                  onUploadError={(error: Error) =>
+                    toast("Erro ao enviar a logo.")
+                  }
+                  appearance={{
+                    label: "text-primary hover:text-primary/60 transition",
+                    button: "bg-primary text-background",
+                    allowedContent: "text-primary/60",
+                    uploadIcon: "text-primary",
+                    container: "w-full h-full -mt-2",
+                  }}
+                  className="border border-primary hover:border-primary/60"
+                />
+              </div>
             </FormControl>
             <FormMessage />
           </FormItem>
