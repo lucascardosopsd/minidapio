f6acefa6604726943e5e8966919963e3fe1ab0fa
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-07-08 01:01:39 -0300

    update: landing page -> infinity moving cards to show restaurants

--- src/app/(public)/(landingPage)/page.tsx
+++ src/app/(public)/(landingPage)/page.tsx
@@ -1,13 +1,34 @@
+import { fetchManyRestaurants } from "@/actions/restaurant/fetchManyRestaurants";
 import Footer from "@/components/landingPage/Footer";
 import AdvantagesSection from "@/components/landingPage/sections/Advantages";
 import HeroSection from "@/components/landingPage/sections/Hero";
 import HowWorksSection from "@/components/landingPage/sections/HowWorks";
 import PlansSection from "@/components/landingPage/sections/Plans";
+import { InfiniteMovingCards } from "@/components/ui/infinity-moving-cards";
 
-const LandingPage = () => {
+const LandingPage = async () => {
+  const { restaurants } = await fetchManyRestaurants({ page: 0, take: 20 });
+
+  const carouselItems = restaurants.map((restaurant) => ({
+    imageUrl: restaurant.logo,
+    href: `${process.env.NEXT_PUBLIC_HOST}/${restaurant.userId}/${restaurant.slug}`,
+    title: restaurant.title,
+  }));
+
   return (
     <>
       <HeroSection />
+      <div className="flex flex-col py-24 border border-boder rounded gap-10">
+        <div className="flex flex-col">
+          <p className="text-center text-2xl text-primary font-semibold">
+            Restaurantes
+          </p>
+          <p className="text-center">
+            Conheça alguns dos nossos restaurantes parceiros
+          </p>
+        </div>
+        <InfiniteMovingCards items={carouselItems} direction="left" />
+      </div>
       <HowWorksSection />
       <AdvantagesSection />
       <PlansSection />
---
+++ src/components/ui/infinity-moving-cards.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+interface InfiniteMovingCardsProps {
+  items: {
+    imageUrl: string;
+    href: string;
+    title?: string;
+  }[];
+  direction?: "left" | "right";
+  speed?: "fast" | "normal" | "slow";
+  pauseOnHover?: boolean;
+  className?: string;
+}
+
+export const InfiniteMovingCards = ({
+  items,
+  direction = "left",
+  speed = "fast",
+  pauseOnHover = true,
+  className,
+}: InfiniteMovingCardsProps) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "80s");
+      }
+    }
+  };
+
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        "scroller relative z-20  max-w-7xl overflow-hidden w-full mx-auto",
+        className
+      )}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          " flex min-w-full shrink-0 gap-10 py-4 w-max flex-nowrap",
+          start && "animate-scroll ",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}
+      >
+        {items.map((item, idx) => (
+          <div className="flex flex-col justify-center items-center">
+            <Image
+              width={170}
+              height={1}
+              src={item.imageUrl}
+              alt={item.href}
+              className="relative rounded-full object-cover w-32 h-32"
+              key={item.href}
+              style={{
+                WebkitFilter: "saturate(0%) contrast(150%)",
+              }}
+            />
+            <p className="text-center font-medium">{item.title}</p>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
