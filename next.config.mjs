eaaf49e7774722f5d0bbcdde87bdc428fbf46b11
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-07-19 08:58:05 -0300

    update: CORS policy

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
       {
         source: "/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Credentials", value: "false" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
