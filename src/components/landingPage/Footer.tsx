25557af0cf5da07a196ca773b0f28cb1e8fd5549
lucas cardoso <lucascardosopsd@gmail.com>
2024-08-28 02:20:14 -0300

    update: terms of use

--- src/components/landingPage/Footer.tsx
+++ src/components/landingPage/Footer.tsx
@@ -6,15 +6,31 @@ const Footer = () => {
       <div className="container flex flex-col tablet:flex-row items-center justify-between mx-auto w-full gap-5 ">
         <div className="flex items-center gap-2">
           <p className="text-2xl font-bold">Minidapio</p>
+
+          <p>|</p>
+
+          <p>
+            Feito por{" "}
+            <Link
+              href="https://www.linkedin.com/in/lcpsd/"
+              className="text-red-500"
+            >
+              Lucas Cardoso
+            </Link>
+          </p>
         </div>
+
         <div className="flex gap-5">
           <div className="flex flex-col">
             <p className="text-2xl font-semibold">Legal</p>
-            <p>Termos de uso</p>
+
+            <Link href="/termos_de_uso.pdf">
+              <p>Termos de Uso</p>
+            </Link>
+
             <Link href="/politica_de_privacidade.pdf">
               <p>Política de Privacidade</p>
             </Link>
-            <p>Cookies</p>
           </div>
 
           <div className="flex flex-col">
