6237d6363010437f2b2555c50459ccf5945182b4
lucas cardoso <lucascardosopsd@gmail.com>
2024-09-05 13:32:15 -0300

    feat: enable or disable category

--- src/validators/category.ts
+++ src/validators/category.ts
@@ -3,5 +3,6 @@ import { z } from "zod";
 export const categoryValidator = z.object({
   title: z.string({ required_error: "Campo Obrigatório" }),
   order: z.number().nullable().default(0),
+  active: z.boolean().default(true).optional(),
   restaurantId: z.string().nullable().optional(),
 });
