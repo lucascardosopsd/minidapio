7a340ed0411a45d865d1a821fd93d4566cdcbd38
lucas cardoso <lucascardosopsd@gmail.com>
2024-09-08 00:49:56 -0300

    fix: delete account logic

--- src/components/settings/Panel.tsx
+++ src/components/settings/Panel.tsx
@@ -15,6 +15,11 @@ import PaymentsHistoryCard from "./PaymentsHistoryCard";
 import UpdateProfileCard from "../restaurant/Profile";
 import { User } from "@prisma/client";
 import { PaymentWithSubscriptionWithPlan } from "@/types/subscription";
+import { deleteUser } from "@/actions/user/deleteUser";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { revalidateRoute } from "@/actions/revalidateRoute";
+import { signOut } from "next-auth/react";
 
 interface SettingsPanelProps {
   checkPayment: CheckMonthlyPaymentReturnProps;
@@ -27,9 +32,23 @@ const SettingsPanel = ({
   user,
   payments,
 }: SettingsPanelProps) => {
+  const router = useRouter();
+
+  const handleDeleteAccount = async () => {
+    try {
+      signOut({ redirect: true, callbackUrl: "/" });
+
+      await deleteUser({ id: user?.id! });
+
+      revalidateRoute({ fullPath: "/" });
+    } catch (error) {
+      toast.error("Ocorreu um erro ao deletar a conta. Contate o suporte");
+    }
+  };
+
   return (
     <section className="flex flex-col tablet:flex-row min-h-screen gap-5 mx-auto">
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 flex-1">
         <SubscriptionCard
           isValidSubscription={
             (checkPayment?.remaining &&
@@ -62,12 +81,13 @@ const SettingsPanel = ({
               content={<></>}
               title="Deletar conta"
               description="Você está prestes a deletar sua conta, tem certeza que deseja continuar?"
+              onSubmit={handleDeleteAccount}
             />
           </CardContent>
         </Card>
       </div>
 
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 flex-1">
         <PaymentsHistoryCard payments={payments} />
 
         <UpdateProfileCard
